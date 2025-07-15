89c51e4b33dc30251b1aeb41fc120dec59aa84e8
GACLove <peng.gaoc@gmail.com>
2025-07-15 23:06:03 +0800

    fix: import lib

--- __init__.py
+++ __init__.py
@@ -1,3 +1,11 @@
-from .nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+import os
+import sys
+from pathlib import Path
+
+current_path = Path(__file__).parent.absolute()
+print("Current path set to:", current_path)
+sys.path.insert(0, os.path.join(current_path, "lightx2v"))  # Adjust the path as needed
+
+from .nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS  # noqa: E402
 
 __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
